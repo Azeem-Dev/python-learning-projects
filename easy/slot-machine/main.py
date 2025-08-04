75f9b3fe4af1a1b2866837d4725dc6e77c01ad7f
Azeem Sharif <azeemsharif5822@gmail.com>
2025-08-04 12:54:30 +0500

    Update main.py

--- easy/slot-machine/main.py
+++ easy/slot-machine/main.py
@@ -65,11 +65,22 @@ def main():
 
         payout = get_payout(row, bet)
 
-        if payout > 0:
-            balance += payout
-            print(f"You won ${payout}! Your new balance is ${balance}.\n")
+        balance += payout
 
-    print("Thank you for playing! Your final balance is $0.")
+        if payout > 0:
+            print(f"You won ${payout}! Your new balance is ${balance}.\n")
+        else:
+            print(f"You lost ${bet}. Your new balance is ${balance}.\n")
+
+        if balance > 0:
+            play_again = input("Do you want to play again? (y/n): ").upper()
+
+        if play_again != "Y" or balance == 0:
+            break
+
+    print("***********************************************************")
+    print(f"Thank you for playing! Your final balance is ${balance}.")
+    print("***********************************************************")
 
 
 if __name__ == '__main__':
