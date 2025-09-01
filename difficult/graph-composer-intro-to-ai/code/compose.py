94c92ed1567a7dd4fc2d64f7ea0ea3ec8c2c7735
Azeem Sharif <azeemsharif5822@gmail.com>
2025-09-01 17:49:39 +0500

    graph composer

--- difficult/graph-composer-intro-to-ai/code/compose.py
+++ difficult/graph-composer-intro-to-ai/code/compose.py
@@ -1,65 +1,87 @@
-"""
-Implemented Markov Chain Composer Graph object by Kylie Ying
-
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-"""
-
 import random
-
-class Vertex(object):
-    def __init__(self, value):
-        self.value = value
-        self.adjacent = {}  # nodes that it points to
-        self.neighbors = []
-        self.neighbors_weights = []
-
-    def __str__(self):
-        return self.value + ' '.join([node.value for node in self.adjacent.keys()])
-
-    def add_edge_to(self, vertex, weight=0):
-        self.adjacent[vertex] = weight
-
-    def increment_edge(self, vertex):
-        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
-
-    def get_adjacent_nodes(self):
-        return self.adjacent.keys()
-
-    # initializes probability map
-    def get_probability_map(self):
-        for (vertex, weight) in self.adjacent.items():
-            self.neighbors.append(vertex)
-            self.neighbors_weights.append(weight)
-
-    def next_word(self):
-        return random.choices(self.neighbors, weights=self.neighbors_weights)[0]
+import re
+import string
+from graph import Graph, Vertex
+import os
 
 
+def get_words_from_text(text_path):
+    with open(text_path, 'r') as file:
+        text = file.read()
 
-class Graph(object):
-    def __init__(self):
-        self.vertices = {}
+        text = re.sub(r'\[(.+)\]', '', text)
 
-    def get_vertex_values(self):
-        return set(self.vertices.keys())
+        text = ' '.join(text.split())
+        """
+        THE ABOVE LINE OF CODE I.E.  -> text = ' '.join(text.split())
+        WHAT IT DOES IS THAT REMOVE ALL/ANY WHITESPACES, NEW LINES, TABS ETC TO JUST A SIMPLE WHITE SPACE.
+        
+        EXAMPLE:
+        
+        text that looks
+        like
+        
+        this
+        
+        NEW BECOMES:
+        
+        text that looks like this
+        
+        """
+        text = text.lower()
 
-    def add_vertex(self, value):
-        self.vertices[value] = Vertex(value)
+        text = text.translate(str.maketrans('', '', string.punctuation))
+    words = text.split()
+    return words
 
-    def get_vertex(self, value):
-        if value not in self.vertices:
-            self.add_vertex(value)
-        return self.vertices[value]
 
-    def get_next_word(self, current_vertex):
-        return self.vertices[current_vertex.value].next_word()
+def make_graph(words):
+    g = Graph()
 
-    def generate_probability_mappings(self):
-        for vertex in self.vertices.values():
-            vertex.get_probability_map()
+    previous_word: Vertex | None = None
+    for word in words:
+        word_vertex = g.get_vertex(word)
+        if previous_word:
+            previous_word.increment_edge(word_vertex)
+
+        previous_word = word_vertex
+
+    g.generate_probability_mappings()
+
+    return g
+
+
+def compose(g, words, length=50):
+    composition = []
+    word = g.get_vertex(random.choice(words))
+    for _ in range(length):
+        composition.append(word.value)
+        word = g.get_next_word(word)
+
+    return composition
+
+
+def main(artist: str):
+
+    words = []
+
+    """FOR SONGS"""
+    for song_file in os.listdir(f'songs/{artist}'):
+        if song_file == '.DS_Store':
+            continue
+
+        song_words = get_words_from_text(f'songs/{artist}/{song_file}')
+        words.extend(song_words)
+
+    """FOR TEXTS"""
+    # words = get_words_from_text('texts/hp_sorcerer_stone.txt')
+
+    g = make_graph(words)
+
+    compostion = compose(g, words, 100)
+
+    return ' '.join(compostion)
+
+
+if __name__ == "__main__":
+    print(main('halsey'))
